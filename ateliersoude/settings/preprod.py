8d9848620f60df8ac9a6bf9f2316eb0ed1e68cff
lucien <lucien@hashbang.fr>
2019-04-30 17:49:48 +0200

    fixed preprod

--- ateliersoude/settings/preprod.py
+++ ateliersoude/settings/preprod.py
@@ -5,3 +5,14 @@ SECRET_KEY = "H/hXAUnb1ZKNGpToim2cg38dxiyHM6b+zB9zozhpTzkP"
 DEBUG = False
 
 ALLOWED_HOSTS = ["ateliersoude.hashbang.fr"]
+
+STATIC_ROOT = "/srv/app/ateliersoude/static/"
+MEDIA_ROOT = "/srv/app/ateliersoude/media/"
+
+INSTALLED_APPS += ["raven.contrib.django.raven_compat"]  # noqa
+
+
+RAVEN_CONFIG = {
+    "dsn": "http://0e041413044f46ff86cedea29c38048d:"
+    "b0a68625ae8b4f85bb4e3891a98475b4@sentry.hashbang.fr/40"
+}
