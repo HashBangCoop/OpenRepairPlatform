e53a311b5522769c4bc5f44081111c297df85959
Hugo <hugo@hashbang.fr>
2019-05-06 10:52:47 +0200

    send emails to orga when an event is published

--- ateliersoude/event/tests/test_commands.py
+++ ateliersoude/event/tests/test_commands.py
@@ -1,19 +1,11 @@
 import datetime
 
 import pytest
+from django.core import mail
 from django.core.management import call_command
 from django.utils import timezone
 
 pytestmark = pytest.mark.django_db
-
-sendmail_count = 0
-
-
-def mocked_send_mail(*args, **kwargs):
-    subject, plain, from_email, recipients = args
-    _ = kwargs.pop("html_message")
-    global sendmail_count
-    sendmail_count += 1
 
 
 def test_command_notify_next_day_events(published_event_factory, user_log):
@@ -21,19 +13,18 @@ def test_command_notify_next_day_events(published_event_factory, user_log):
         starts_at=timezone.now() + datetime.timedelta(days=1),
     )
     event1.registered.add(user_log)
-    from django.core import mail
-    mail.send_mail = mocked_send_mail
     call_command("notify_next_day_events", "https://example.com")
-    global sendmail_count
-    assert sendmail_count == 1
-    sendmail_count = 0
+    assert len(mail.outbox) == 1
 
 
-def test_command_publish_events(event_factory):
+def test_command_publish_events(event_factory, custom_user_factory):
     event1 = event_factory(
         published=False,
         publish_at=timezone.now() - timezone.timedelta(hours=1)
     )
-    call_command("publish_events")
+    event1.organization.admins.add(custom_user_factory())
+    event1.organization.volunteers.add(custom_user_factory())
+    call_command("publish_events", "https://example.com")
     event1.refresh_from_db()
+    assert len(mail.outbox) == 2
     assert event1.published
