d47c509d3162c143211c604838c5f566ea90fd63
Hugo <hugo@hashbang.fr>
2019-06-12 10:15:01 +0200

    Add dockers

--- ateliersoude/settings/prod.py
+++ ateliersoude/settings/prod.py
@@ -1,18 +1,38 @@
-from os import environ
+import os
 from .base import *  # noqa
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "H/hXAUnb1ZKNGpToim2cg38dxiyHM6b+zB9zozhpTzkP"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 DEBUG = False
 
-EMAIL_ADRESSE = environ.get("EMAIL_ADRESSE")
+ALLOWED_HOSTS = [
+    "127.0.0.1", "localhost", "atelier-soude.fr", "www.atelier-soude.fr",
+]
 
-EMAIL_USE_TLS = True
-EMAIL_HOST = environ.get("SMTP_HOST")
-EMAIL_HOST_USER = EMAIL_ADRESSE
-EMAIL_HOST_PASSWORD = environ.get("EMAIL_PASSWORD")
-EMAIL_PORT = 587
+STATIC_ROOT = "/srv/static/"
+MEDIA_ROOT = "/srv/media/"
+ASSETS_ROOT = STATIC_ROOT
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "ateliersoude",
+        "USER": "ateliersoude",
+        "HOST": "postgres",
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "ateliersoude"),
+    }
+}
+
+raven = os.getenv("RAVEN_DNS")
+
+if raven:
+    RAVEN_CONFIG = {"dsn": raven}
+    INSTALLED_APPS += ["raven.contrib.django.raven_compat"]  # noqa
+
+# Email Settings
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-DEFAULT_FROM_EMAIL = EMAIL_ADRESSE
-SERVER_EMAIL = EMAIL_ADRESSE
+EMAIL_HOST = "mail.atelier-soude.fr"
+EMAIL_PORT = 25
+EMAIL_HOST_USER = "no-reply@atelier-soude.fr"
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
